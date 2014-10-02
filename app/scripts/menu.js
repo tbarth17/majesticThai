a2026086df355e9ed3c4bfd464a1d9ba54d83e59
Tommy Barth <tommyhbrc@gmail.com>
2014-10-02 17:41:41 -0400

    Initial Commit

---
+++ app/scripts/menu.js
@@ -0,0 +1,37 @@
+var menu = [
+  {
+    category: 'Salads',
+    items: [
+      {
+        title: 'Koi pla',
+        description: 'Minced or finely chopped raw fish in spicy salad dressing.',
+        price: 9,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Koi_pla.png/800px-Koi_pla.png'
+      },
+      {
+        title: 'Lap Isan',
+        description: 'Northeastern style lap is a spicy and sour salad of minced raw or cooked meat (mainly pork, chicken or duck), shallots or onions, lime juice, fish sauce, chillies, ground roasted rice and mint.',
+        price: 10,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Lap_mu_isan.JPG/1024px-Lap_mu_isan.JPG'
+      },
+      {
+        title: 'Lap nuea dip',
+        description: 'A northern Thai lap-style salad of sliced raw beef (the version in the image also shows slices of raw beef tripe) and ground, dried spices.',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Lap_nuea_dip.jpg/800px-Lap_nuea_dip.jpg'
+      },
+      {
+        title: 'Naem khluk',
+        description: 'Naem sausage (pork fermented with sticky rice)[13] is mixed with boiled rice and then deep-fried. The salad is made by crumbling the deep-fried balls and mixing in sliced shallots, dried chillies, fish sauce and lime juice. It is served with raw vegetables and herbs.',
+        price: 9,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Naem_khluk_1.jpg/640px-Naem_khluk_1.jpg'
+      },
+      {
+        title: 'Phla kung',
+        description: 'A spicy salad of prawns, sliced lemongrass, culantro and/or other herbs, and shallots, with a dressing of lime juice, sweet chilli paste (Nam phrik phao - optional), fish sauce, pounded garlic and birds eye chili. The version shown in the image also contained minced pork.',
+        price: 10,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Phla_kung.jpg/640px-Phla_kung.jpg'
+      }
+    ]
+  }
+]
