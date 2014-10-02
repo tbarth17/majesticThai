3311ecf0e0239949d48817136db8c2a0ec7fc84b
Tommy Barth <tommyhbrc@gmail.com>
2014-10-02 18:14:51 -0400

    Second commit, expands menu item database

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -33,5 +33,137 @@ var menu = [
         imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Phla_kung.jpg/640px-Phla_kung.jpg'
       }
     ]
+  },
+  {
+    category: 'Appetizers',
+    items: [
+      {
+        title: 'Chingrit thot',
+        description: 'Deep-fried crickets, either Gryllus bimaculatus or, as shown in the image, Acheta domesticus. This dish is often eaten as a snack to go with drinks.',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chingrit_thot.jpg/640px-Chingrit_thot.jpg'
+      },
+      {
+        title: 'Kai rom khwan',
+        description: 'Smoked chicken is often eaten as a snack to go with drinks.',
+        price: 9,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Kai_rom_khwan.jpg/640px-Kai_rom_khwan.jpg'
+      },
+      {
+        title: 'Karipap',
+        description: 'Deep-fried pastry shells usually containing a chicken meat and potato curry.',
+        price: 7,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Karipap_Chiang_Mai.jpg/640px-Karipap_Chiang_Mai.jpg'
+      },
+      {
+        title: 'Khaep mu',
+        description: 'Deep fried crispy pork rinds, often eaten with nam phrik num and other northern Thai dips',
+        price: 7,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Khaep_mu.jpg/640px-Khaep_mu.jpg'
+      },
+      {
+        title: 'Khanom chip',
+        description: 'The Thai version of the Chinese steamed dumplings called siu mai in Cantonese. The colour indicates the filling: the green dumplings contain a mix of minced pork and crab meat, the others have a filling of only minced pork.',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Khanom_chip.jpg/640px-Khanom_chip.jpg'
+      },
+      {
+        title: 'Khanom kuichai',
+        description: 'Originally a dish of the Teochew people called "gu chai gue" (Chinese script: 韭菜馃) in the Teochew language, these are steamed dumplings made from rice powder and a filling of garlic chives. The dipping sauce for this dish is soy sauce which often is spiced with dried chilli flakes. This particular version was fried to give it a crispy texture.',
+        price: 9,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Khanom_kuichai.jpg/320px-Khanom_kuichai.jpg'
+      }
+    ]
+  },
+  {
+    category: 'Soups',
+    items: [
+      {
+        title: 'Kai tun ya chin',
+        description: 'The name literally translates to "chicken stewed with Chinese medicine". It contains medicinal herbs, one of them the dried fruit of the wolfberry, a.k.a. goji berries (Thai: เก๋ากี้; kaoki). The dish is of Chinese origin.',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Kai_tun_ya_chin.JPG/320px-Kai_tun_ya_chin.JPG'
+      },
+      {
+        title: 'Tom chuet',
+        description: 'A light vegetable, chicken or pork broth with vegetables and celery, to which minced pork, soft tofu, seaweed, glass noodles and mushroom can be added. This soup can also be called Kaeng chuet (Thai: แกงจืดวุ้นเส้น).',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Tom_jued_tahoo.jpg/1024px-Tom_jued_tahoo.jpg'
+      },
+      {
+        title: 'Tom kha kai',
+        description: 'A mild to spicy soup with coconut milk, galangal (kha) and chicken. Mushrooms can be added to the chicken or substitute it.',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/en/c/c8/Tom_kha_gai.jpg'
+      },
+      {
+        title: 'Tom khlong',
+        description: 'It is a spicy and sour soup somewhat similar to Tom yam. The sourness however does not derive from lime juice but through the use of tamarind juice. The version in the image contains fried smoked fish, tomato and mushrooms.',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Tom_khlong.JPG/1024px-Tom_khlong.JPG'
+      },
+      {
+        title: 'Tom mara',
+        description: 'A clear soup made with bitter melon (also known as bitter gourd; Momordica charantia) of Chinese origin. It is often stuffed with minced pork or with minced pork mixed with glass noodles as on this image. The full name of the latter version would then be tom mara yat sai mu sap wunsen.',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Tom_mara_yat_sai_mu_sap_wunsen.jpg/640px-Tom_mara_yat_sai_mu_sap_wunsen.jpg'
+      },
+      {
+        title: 'Tom saep',
+        description: 'A spicy soup made with stewed meat (usually pork, chicken or beef), roasted fresh herbs and spices, ground roasted rice, and generous amounts of lime juice and fresh herbs just before serving.',
+        price: 8,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Tom_saep.JPG/110px-Tom_saep.JPG'
+      },
+    ]
+  },
+  {
+    category: 'Curries',
+    items:
+    [
+      {
+        title: 'Chin hum',
+        description: 'It is a slowly stewed meat curry from northern Thailand. This particular version is made with beef.',
+        price: 10,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Chin_hum.jpg/640px-Chin_hum.jpg'
+      },
+      {
+        title: 'Chuchi pla thu sot',
+        description: 'A semi-dry curry made with fresh (i.e. unsteamed and unsalted) pla thu (Rastrelliger brachysoma).',
+        price: 12,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Chuchi_pla_thu_sot.jpg/1024px-Chuchi_pla_thu_sot.jpg'
+      },
+      {
+        title: 'Kaeng cha-om',
+        description: 'A northern Thai curry with cha-om (the young leaves of the Acacia pennata) and dried fish.[5] This particular version also contained a mix of different types of mushroom.',
+        price: 11,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Kaeng_chaom.JPG/640px-Kaeng_chaom.JPG'
+      },
+      {
+        title: 'Kaeng fak sai kai',
+        description: 'A northern Thai curry made with winter melon and chicken. It is also known under the name kaeng fak khio.',
+        price: 12,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Kaeng_fak_sai_kai.JPG/640px-Kaeng_fak_sai_kai.JPG'
+      },
+      {
+        title: 'Kaeng het',
+        description: 'A northern Thai curry made with pork and a variety of mushrooms.',
+        price: 11,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Kaeng_het.jpg/640px-Kaeng_het.jpg'
+      },
+      {
+        title: 'Kaeng phanaeng',
+        description: 'A so-called dry, Indian influenced coconut curry with beef (phanaeng nuea, Thai: พะแนงเนื้อ), chicken, pork or seafood such as soft shell crab.',
+        price: 12,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Phanaeng_kai.jpg/640px-Phanaeng_kai.jpg'
+      },
+      {
+        title: 'Kaeng phet pet yang',
+        description: 'Red curry with roast duck is the quintessential mix of the Thai (red curry) and Chinese (red roast duck) cuisines. This dish often also contains grapes and/or pineapple.',
+        price: 13,
+        imgUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Red_roast_duck_curry.jpg/1024px-Red_roast_duck_curry.jpg'
+      }
+    ]
   }
-]
+
+
+];
